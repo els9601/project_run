5213ce1163eaafa968213e0111bc6c8ca6aa6e7d
run_app <els96@icloud.com>
2026-05-01 20:41:56 +0300

    model Run

--- project_run/urls.py
+++ project_run/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from run_app.views import company_details
+from rest_framework.routers import DefaultRouter
+from run_app.views import RunViewSet
+
+router = DefaultRouter()
+router.register('api/runs', RunViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/company_details/', company_details),
-]
+    path('', include(router.urls))
+    ]
+
+
--- run_app/views.py
+++ run_app/views.py
@@ -1,6 +1,9 @@
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
+from .serializers import RunSerializer
+from .models import Run
 from django.shortcuts import render
 
 @api_view(['GET'])
@@ -9,3 +12,8 @@ def company_details(request):
                'slogan': settings.SLOGAN,
                'contacts': settings.CONTACTS}
     return Response(details)
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
+
