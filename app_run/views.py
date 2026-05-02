f4445bf26cdd28c15f61ddd01281b1e3ed978ae9
run_app <els96@icloud.com>
2026-05-02 20:05:49 +0300

    User + 'coach'/'athlete' + переопределение

--- app_run/views.py
+++ app_run/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
-from .serializers import RunSerializer
+from .serializers import RunSerializer, UserSerializer
 from .models import Run
 from django.shortcuts import render
 
@@ -17,3 +18,15 @@ class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
 
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.filter(is_superuser=False)
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        user_type = self.request.query_params.get('type')
+        if user_type == 'coach':
+            return qs.filter(is_staff=True)
+        elif user_type == 'athlete':
+            return qs.filter(is_staff=False)
+        return qs
--- project_run/urls.py
+++ project_run/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from app_run.views import company_details
+from app_run.views import company_details, UserViewSet
 from rest_framework.routers import DefaultRouter
 from app_run.views import RunViewSet
 
 router = DefaultRouter()
 router.register('api/runs', RunViewSet)
+router.register('api/users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
